20799f1e32d7063ad02c1c6e5236b44eeb78ebaf
Broken-Admin <alexlresu@gmail.com>
2023-09-20 14:47:16 -0500

    Add I2C check .ino

---
+++ embedded/embedded/i2c_check/i2c_check.ino
@@ -0,0 +1,71 @@
+// --------------------------------------
+// i2c_scanner
+//
+// Modified from https://playground.arduino.cc/Main/I2cScanner/
+// --------------------------------------
+
+#include <Wire.h>
+
+// Set I2C bus to use: Wire, Wire1, etc.
+#define WIRE Wire
+
+void setup() {
+  WIRE.setSDA(18);
+  WIRE.setSCL(19);
+  WIRE.begin();
+
+  pinMode(25, OUTPUT);
+  digitalWrite(25, HIGH);
+
+  Serial.begin(9600); // Open serial line TODO: Remove this in favor of xbee
+  Serial2.setTX(8);
+  Serial2.setRX(9);
+  Serial2.begin(9600); // Start Xbee Line
+  while (!Serial)
+     delay(10);
+  Serial.println("\nI2C Scanner");
+  
+}
+
+
+void loop() {
+  byte error, address;
+  int nDevices;
+
+  Serial.println("Scanning...");
+
+  nDevices = 0;
+  for(address = 1; address < 127; address++ ) 
+  {
+    // The i2c_scanner uses the return value of
+  // the Write.endTransmisstion to see if
+    // a device did acknowledge to the address.
+    WIRE.beginTransmission(address);
+    error = WIRE.endTransmission();
+
+    if (error == 0)
+    {
+      Serial.print("I2C device found at address 0x");
+      if (address<16) 
+        Serial.print("0");
+      Serial.print(address,HEX);
+      Serial.println("  !");
+
+      nDevices++;
+    }
+    else if (error==4) 
+    {
+      Serial.print("Unknown error at address 0x");
+      if (address<16) 
+        Serial.print("0");
+      Serial.println(address,HEX);
+    }    
+  }
+  if (nDevices == 0)
+    Serial.println("No I2C devices found\n");
+  else
+    Serial.println("done\n");
+
+  delay(5000);           // wait 5 seconds for next scan
+    // waits 15ms for the servo to reach the position
+}
