9a54442aec27ef7fa4dc27cedc4f1431674c8725
broken-admin <brokengaming50@gmail.com>
2023-10-16 07:55:14 +0000

    Update index.js * Update ROS2 node implementation * Implement Express & Socket.io * Socket.io events communicated to ROS2 subscribers

--- basestation/basestation/src/index.js
+++ basestation/basestation/src/index.js
@@ -1,43 +1,117 @@
 const rclnodejs = require('rclnodejs');
+const express = require('express');
+const app = express();
+const http = require('http');
+// Create an http server using the expess application
+const server = http.createServer(app);
+// Socket.io integration
+const { Server } = require("socket.io");
+const io = new Server(server);
 
+// Special Basestation class extending the Node
 class Basestation extends rclnodejs.Node {
   constructor() {
     super('Basestation');
 
     // Do webserver initalization
 
-    // Create ROS2 publisher and subscribers
+    // Create ROS2 publisher
+    // Parsed by Libre node
+    // Parsed data via serial to the Pico
+    this.publisher = this.createPublisher(
+      'std_msgs/msg/String',
+      '/roverone/control_data'
+    );
+
+    // Create a ROS2 subscriber
+    // Output from the Pico, parsed by the Libre
+    // Libre parsed data sent via ROS2
+    this.subscriber = this.createSubscription(
+      'std_msgs/msg/String',
+      '/roverone/pico_status',
+      this.handler
+    );
+
+    this.echo = this.createSubscription(
+      'std_msgs/msg/String',
+      '/roverone/control_data',
+      this.echoHandler
+    );
   }
 
-  // Write methods
+  // Handle sending data
+  // String data
+  // Create a format
+  publish(data) {
+    this.publisher.publish(data);
+  }
+
+  // Subscriber handler
+  // Integrate socket.io
+  handler(msg) {
+    console.log(`Recieved message: ${typeof msg}`, msg.data);
+  }
+
+  // Handles output from this node to be echoed
+  echoHandler(msg) {
+    console.log(`This node sent data: ${msg.data}`);
+  }
 }
 
-// Create a node that publishes a msg to the topic 'foo' every 1 second.
-// View the topic from the ros2 commandline as shown below:
-//    ros2 topic echo foo std_msgs/msg/String
-async function example() {
+
+// Initalize the ROS2 node for use in the socket
+var basestationNode;
+
+// ROS2 Node handling
+async function ROS2_handle() {
+  // Initalize the ROS2 master
   await rclnodejs.init();
-  const node = rclnodejs.Node('Basestation');
 
-  // Create main working components here, e.g., publisher, subscriber, service, client, action
-  // For example, a publisher sending a msg every 1 sec
-  const publisher = node.createPublisher('std_msgs/msg/String', 'foo');
-  let cnt = 0;
-  const msg = rclnodejs.createMessageObject('std_msgs/msg/String');
-  node.createTimer(1000, () => {
-    msg.data = `msg: ${
-      cnt += 1
-    }`;
-    publisher.publish(msg);
-  });
+  basestationNode = new Basestation();
+  // Test data publish
+  basestationNode.publish('Some fucking data');
 
-  node.spin();
+  // Initialize the ROS2 node
+  basestationNode.spin();
 
-  console.log('Use this command to view the node\'s published messages: ros2 topic echo foo std_msgs/msg/String');
+  console.log('Use this command to view the node\'s published messages: ros2 topic echo /roverone/control_data std_msgs/msg/String');
 }
 
+// ROS2 handler initialization
 (async function main() {
-  example();
+  ROS2_handle();
 }()).catch(() => {
   process.exitCode = 1;
+});
+
+/* WEB SOCKET HANDLERS */
+/*
+ * All web page hosting performed
+ * after the ROS2 node has been intialized and spun
+*/
+/***********************/
+
+// Initialize the static web page, updated via the socket connection
+app.use(express.static(__dirname + '/www'));
+
+// Socket handlers
+io.on('connection', (socket) => {
+  console.log('Connection');
+
+  // Disconnection event handler
+  socket.on('disconnect', () => {
+    console.log('Disconnection.');
+  });
+
+  // Returned controller data update
+  socket.on('controller_data', (data) => {
+    console.log(`Received data from the web page: ${data}`);
+    basestationNode.publish(data);
+  });
+});
+
+// Listen for connections on the public IP address
+// Serve the used web directory
+server.listen(3000, '0.0.0.0', () => {
+  console.log('listening on *:3000');
 });
