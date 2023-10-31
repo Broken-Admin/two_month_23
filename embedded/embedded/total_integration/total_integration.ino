d55b2bd949325d7394066f3dbc116fde6a275533
Broken-Admin <alexlresu@gmail.com>
2023-10-31 17:23:39 -0500

    Integrate all peripherals in total_integration.ino + BMP388 + BNO055 + Openlog * Update Pico to wait for "ping" before initalizing and running code

--- embedded/embedded/total_integration/total_integration.ino
+++ embedded/embedded/total_integration/total_integration.ino
@@ -1,4 +1,13 @@
+// Servo Control
 #include <Servo.h>
+// I2C
+#include <Wire.h>
+// Sensor Libraries
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BNO055.h>
+#include <Adafruit_BMP3XX.h>
+// Sensor-associated libraries  
+#include <utility/imumaths.h>
 
 // #define GREEN_LED 2
 // #define RED_LED 3
@@ -26,6 +35,17 @@
 #define LT_SIDE 90 // 3:00
 #define LT_DOWN 0 // 6:00
 
+// Interval definitions
+#define BNO055_CHECK_INTERVAL 2500
+#define BMP388_CHECK_INTERVAL 2000
+#define READY_INTERVAL 1800
+
+// BMP constant definitions
+#define SEALEVELPRESSURE_HPA (1013.25)
+
+// Openlogger
+#define Openlogger Serial1
+
 // Moving UP the arm
 Servo Joint1; // Cont
 Servo Joint2; // 270
@@ -34,8 +54,26 @@ Servo Joint4; // 180
 Servo Joint5; // Cont
 Servo Joint6; // 180
 
-char *delim = " \n\t";
+char delim[] = " \n\t";
+
+// BNO055 definitions
+Adafruit_BNO055 bno = Adafruit_BNO055(55, BNO055_ADDRESS_A, &Wire);
 bool bnoAttached = false;
+
+// BMP388 definitions
+Adafruit_BMP3XX bmp;
+bool bmpAttached = false;
+
+
+// Delay definitions
+unsigned long previousBNOMillis = 0;
+unsigned long previousBMPMillis = 0;
+unsigned long previousReadyMillis = 0;
+unsigned long currentMillis = 0;
+int polledSensor;
+
+// Output String
+String dataout = "";
 
 void setup() {
     // Built in LED
@@ -44,6 +82,13 @@ void setup() {
     // Configure USB serial
     // Value provided is ignored in favor for the USB default
     Serial.begin(115200);
+
+    /* Openlogger */
+    // Configure Openlogger serial
+    Openlogger.begin(9600);
+    // Add header to Openlogger
+    Openlogger.println("Orientation X,Orientation Y,Orientation Z,Lin Accel X, Lin Accel Y, Lin Accel Z,Temperature (*C),Pressure (hPa),Approx Altitude (m)");
+
     // Sleep for one second
     while(!Serial);
 
@@ -60,38 +105,81 @@ void setup() {
 
     // End Effector
     // Joint1.write(95); // Continuous
-    Joint2.write(0); 
-    Joint3.write(0);
-    Joint4.write(0);
+    Joint2.write(0); // HT 
+    Joint3.write(0); // LT
+    Joint4.write(0); // LT
     // Joint5.write(95); // Continuous
-    Joint6.write(0);
+    Joint6.write(0); // LT
+
+    // Wait for a ping
+    while(true) {
+        if(Serial.available()) {
+            String command = Serial.readStringUntil('\n');
+            command.trim();
+            if(command == "ping") {
+                // Return the message 
+                Serial.println("pong");
+                // Break out of the catch loop
+                break;
+            }
+        }
+    }
 
     // BNO055 Integration
     /* Initialise the sensor */
     if(!bno.begin())
     {
         /* There was a problem detecting the BNO055 ... check your connections */
-        Serial.print("[ERROR] No BNO055 detected");
+        Serial.println("[ERROR] No BNO055 detected");
     } else {
         bnoAttached = true;
         bno.setExtCrystalUse(true);
     }
-    
+
+    if(!bmp.begin_I2C(BMP3XX_DEFAULT_ADDRESS, &Wire)) {
+        Serial.println("[ERROR] No BMP380 detected");
+    } else {
+        bmpAttached = true;
+        bmp.setTemperatureOversampling(BMP3_OVERSAMPLING_8X);
+        bmp.setPressureOversampling(BMP3_OVERSAMPLING_4X);
+        bmp.setIIRFilterCoeff(BMP3_IIR_FILTER_COEFF_3);
+        bmp.setOutputDataRate(BMP3_ODR_50_HZ);
+    }
 }
 
 void loop() {
-    Serial.println("[INFO] Ready for Input");
+    polledSensor = 0;
+
+    // Update interval
+    currentMillis = millis();
+
+    // Reset data string, if within a time interval
+    if(currentMillis - previousBNOMillis >= BNO055_CHECK_INTERVAL || currentMillis - previousBMPMillis >= BMP388_CHECK_INTERVAL) {
+        dataout = "";
+    }
+
+    // Let user know dataready
+    if(currentMillis - previousReadyMillis >= READY_INTERVAL) {
+        Serial.println("[GOOD] Ready for Input");
+        previousReadyMillis = currentMillis;
+    }
+
 
     // Command processing
     if (Serial.available()) {
         String serialData = Serial.readStringUntil('\n');
         char charData[serialData.length()];
         serialData.toCharArray(charData, serialData.length());
-        // Parse the command
+        // Parse the command, only properly parses if using crlf (\n\r) or a trailing space
         char *command = strtok(charData, delim);
 
-        String arduCommand(command);
-        Serial.println(arduCommand);
+        // Bring the command to lower case
+        String arduCommand = String(command);
+        arduCommand.toLowerCase();
+        arduCommand.toCharArray(command, arduCommand.length()+1);
+
+        // Halt until "ping"
+        // while(strcmp(command, "ping") != 0);
 
         if (strcmp(command, "led_on") == 0) {
             digitalWrite(LED_BUILTIN, HIGH);
@@ -99,6 +187,10 @@ void loop() {
             digitalWrite(LED_BUILTIN, LOW);
         } else if (strcmp(command, "ping") == 0) {
             Serial.println("pong");
+        } else if (strcmp(command, "hcf") == 0) {
+            // Halt and Catch Fire
+            Serial.println("[WARNING] HALT");
+            while(1);
         } else if (strcmp(command, "servo") == 0) {
         
             char *servo_str = strtok(NULL, delim);
@@ -106,13 +198,16 @@ void loop() {
 
             int servo_n = strtol(servo_str, NULL, 10);
             int angle_n = strtol(angle_str, NULL, 10);
-
+            // Servo Number
             if(servo_n < 1 || servo_n > 6) {
                 Serial.print("[WARNING] Attempted to move unknown servo number ");
             } else {
-                Serial.print("[INFO] Attemping to move servo number ");
+                Serial.print("[GOOD] Attemping to move servo number ");
             }
-            Serial.println(servo_n);
+            Serial.print(servo_n);
+            // Angle
+            Serial.print(" to an angle of ");
+            Serial.println(angle_n);
 
             switch(servo_n) {
                 case 1: // Base: Continuous
@@ -148,23 +243,84 @@ void loop() {
                     break;
             }
         }
+
+        // If the command does not control a servo
+        // output
+        if(strcmp(command, "servo") != 0) {
+            Serial.println(command);
+        }
     }
     
-    if(bnoAttached) {//could add VECTOR_ACCELEROMETER, VECTOR_MAGNETOMETER,VECTOR_GRAVITY...
-        sensors_event_t orientationData , angVelocityData , linearAccelData, magnetometerData, accelerometerData, gravityData;
+    // Regularly check for data and send a message
+    if(bnoAttached && currentMillis - previousBNOMillis >= BNO055_CHECK_INTERVAL) {
+        //could add VECTOR_ACCELEROMETER, VECTOR_MAGNETOMETER,VECTOR_GRAVITY...
+        sensors_event_t orientationData, linearAccelData;
+        // getEvent always returns true, there's no needs 
         bno.getEvent(&orientationData, Adafruit_BNO055::VECTOR_EULER);
         bno.getEvent(&linearAccelData, Adafruit_BNO055::VECTOR_LINEARACCEL);
+
+        dataout += String(orientationData.orientation.x) + ",";
+        dataout += String(orientationData.orientation.y) + ",";
+        dataout += String(orientationData.orientation.z) + ",";
+
+        // Serial.println(String(orientationData.orientation.x) + String(orientationData.orientation.y) + String(orientationData.orientation.z));
+
+        dataout += String(linearAccelData.acceleration.x) + ",";
+        dataout += String(linearAccelData.acceleration.y) + ",";
+        dataout += String(linearAccelData.acceleration.z) + ",";
+
+        // Serial.println(String(linearAccelData.acceleration.x) + String(linearAccelData.acceleration.y) + String(linearAccelData.acceleration.z));
 
         printEvent(&orientationData);
         printEvent(&linearAccelData);
 
-        int8_t boardTemp = bno.getTemp();
-        Serial.println();
-        Serial.print(F("temperature: "));
-        Serial.println(boardTemp);
+        // int8_t boardTemp = bno.getTemp();
+        // Serial.print(F("[INFO] Temperature (deg Celcius): "));
+        // Serial.println(boardTemp);
 
-        // Cannot delay the sampling because it would change the rate at which servos update
-        // delay(BNO055_SAMPLERATE_DELAY_MS);
+        // Reset the interval
+        previousBNOMillis = currentMillis;
+
+        // Update openlogger write flag
+        polledSensor++;
+    } else if(currentMillis - previousBNOMillis >= BNO055_CHECK_INTERVAL) {
+        // Empty columns when the BNO is not attached
+        dataout += ",,,,,,";
+        // Reset the interval
+        previousBNOMillis = currentMillis;
+        // Don't update the write flag
+    }
+
+    if (bmpAttached && currentMillis - previousBMPMillis >= BMP388_CHECK_INTERVAL) {
+        // Attempt to get a reading
+        if (!bmp.performReading()) {
+            Serial.println("[ERROR] BMP388 failed to take reading");
+            dataout += ",,";
+        } else {
+            Serial.print("[INFO] Temperature (*C): ");
+            Serial.println(bmp.temperature);
+            dataout += String(bmp.temperature) + ",";
+
+            Serial.print("[INFO] Pressure (hPa): ");
+            Serial.println(bmp.pressure / 100.0);
+            dataout += String(bmp.pressure / 100.0) + ",";
+
+            Serial.print("[INFO] Approx. Altitude (m): ");
+            Serial.println(bmp.readAltitude(SEALEVELPRESSURE_HPA));
+            dataout += String(bmp.readAltitude(SEALEVELPRESSURE_HPA));
+            
+            // Update openlogger write flag
+            polledSensor++;
+        }
+        
+        // Reset the interval
+        previousBMPMillis = currentMillis;
+    } else if(currentMillis - previousBNOMillis >= BNO055_CHECK_INTERVAL) {
+        // Empty columns when the BMP is not attached
+        dataout += ",,";
+        // Reset the interval
+        previousBMPMillis = currentMillis;
+        // Don't update the write flag
     }
 
     // These are not connected or utilized
@@ -178,17 +334,27 @@ void loop() {
     // digitalWrite(RED_LED, LOW);
     // digitalWrite(BLUE_LED, LOW);
     // delay(250);
+
+    // If data was updated, send it to the Openlogger
+    // This is ONLY run if the sensors are successfully polled 
+    if(polledSensor == 2) {
+        Openlogger.println(dataout);
+        // Serial.println(dataout);
+    }
+
+    // Process data and perform checks every quarter second
+    delay(250);
 }
 
 void printEvent(sensors_event_t* event) {
   double x = -1000000, y = -1000000 , z = -1000000; //dumb values, easy to spot problem
   if (event->type == SENSOR_TYPE_ORIENTATION) {
-    Serial.print("[INFO] Orient:");
+    Serial.print("[INFO] Orient: ");
     x = event->orientation.x;
     y = event->orientation.y;
     z = event->orientation.z;
   } else if (event->type == SENSOR_TYPE_LINEAR_ACCELERATION) {
-    Serial.print("[INFO] Linear:");
+    Serial.print("[INFO] Linear: ");
     x = event->acceleration.x;
     y = event->acceleration.y;
     z = event->acceleration.z;
